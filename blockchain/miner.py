f7cf51a54380fb48eb44e4686463ed224d0ab3a2
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-01-17 11:34:34 -0500

    blockchain code servers are down

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,9 +23,12 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    proof = random.randint(0, sys.maxsize / 1000)
     #  TODO: Your code here
-
+    guess = f"{last_proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    while valid_proof(guess_hash, proof) is False:
+        proof +=1
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
@@ -40,7 +43,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f"{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
